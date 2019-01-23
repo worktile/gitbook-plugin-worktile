52f716e8fb622dc5bc427da5a0a451ce37be1e15
pubuzhixing8 <pubuzhixing@gmail.com>
2019-01-23 16:51:59 +0800

    初始化插件

---
+++ book/toggle.js
@@ -0,0 +1,23 @@
+require(["gitbook", "jQuery"], function (gitbook, $) {
+  gitbook.events.bind("page.change", function () {
+    // $(".gitbook-link").hide();
+    $(".body-inner").append('<div style="text-align: center; padding: 25px; color: rgba(160, 160, 160, 1);">' + icp + '</div>');
+    insertLogo(src, style, link)
+  });
+  var icp = '© Worktile 2018 | 京ICP备13017353号-3';
+  var src = ''
+  var style = ''
+  var link = ''
+  gitbook.events.bind('start', function (e, config) {
+    src = config['xingxing-tinyfold-chapters']['src']
+    style = config['xingxing-tinyfold-chapters']['style']
+    link = config['xingxing-tinyfold-chapters']['link']
+    link = config['xingxing-tinyfold-chapters']['link']
+    icp = config['xingxing-tinyfold-chapters']['icp']
+  })
+
+  var insertLogo = function (src, style, link) {
+    $('ul.summary').children().eq(0).before('<a class="book-logo" target="_blank" href="' + link + '"><img src="' + src + '" style="' + style + '"></a>')
+  }
+
+});
